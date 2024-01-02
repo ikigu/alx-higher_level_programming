e41b9680820b8c7e376aa722e85abfc1dddf6129
George Ikigu <ikigu.geo@gmail.com>
2024-01-02 20:29:19 +0300

    fix: enable script to work with other inputs

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]  # first three letters of word
-word_last_2 = word[7:]  # last two letters of word
+word_last_2 = word[-2:]  # last two letters of word
 middle_word = word[1:len(word) - 1]  # word without first and last letters
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
