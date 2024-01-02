8e8ebc853fe5e78525f6c373133bb34bb6d0db50
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 00:57:44 +0300

    solution: get value of last digit from a random #

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+number_copy = number
+
+if number < 0:
+    number_copy = number * -1
+
+last_digit = number_copy % 10
+prefix = "Last digit of"
+
+if last_digit > 5:
+    suffix = "and is greater than 5"
+elif last_digit < 6 and last_digit != 0:
+    suffix = "and is less than 6 and not 0"
+elif last_digit == 0:
+    suffix = "and is 0"
+
+print(f"{prefix} {number:d} is {last_digit:d} {suffix}")
