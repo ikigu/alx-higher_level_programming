fe3f75e5363420d25b3c78cf4b5e6fa3c3dc210a
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 02:51:24 +0300

    solution: function that prints passed string in upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Write a function that prints a string in uppercase followed by a new line.
+
+-> Prototype: def uppercase(str):
+-> You can only use no more than 2 print functions with string format
+-> You can only use one loop in your code
+-> You are not allowed to import any module
+-> You are not allowed to use str.upper() and str.isupper()
+-> Tips: ord()
+
+"""
+
+
+def uppercase(str: str):
+    for i in range(0, len(str)):
+
+        char_in_ascii: int = ord(str[i])
+
+        if char_in_ascii > 96 and char_in_ascii < 123:
+            char_in_ascii -= 32
+
+        if i != len(str) - 1:
+            print("{char:c}".format(char=char_in_ascii), end="")
+        else:
+            print("{char:c}".format(char=char_in_ascii))
