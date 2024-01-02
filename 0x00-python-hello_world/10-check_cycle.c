2a82c4c39ddf1789eccff69e2a968b48f0693741
George Ikigu <ikigu.geo@gmail.com>
2024-01-02 21:59:55 +0300

    solution: check whether a linked list is cyclic

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * check_cycle - checks whether a linked list is cyclic
+  * @list: head of a linked list
+  *
+  * Return: 0 if there is no cycle, 1 if there is
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	current = current->next;
+
+	while (current)
+	{
+		if (current == list)
+		{
+			return (1);
+		}
+
+		current = current->next;
+	}
+
+	return (0);
+}
+
