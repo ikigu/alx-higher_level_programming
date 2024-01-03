b21afc107f20e0b78c3226b75c2d7c7ea1b7625e
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 03:12:09 +0300

    solution: function that adds two ints, returns result

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Write a function that adds two integers and returns the result.
+
+=> Prototype: def add(a, b):
+=> Returns the value of a + b
+=> You are not allowed to import any module
+
+"""
+
+def add(a: int, b: int) -> int:
+    return a + b
