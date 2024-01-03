5823cba1616ffa88cf9614dfd0d3526d0fcfdf41
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 03:17:05 +0300

    solution: function that returns a^b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Write a function that computes a to the power of b and return the value.
+
+=> Prototype: def pow(a, b):
+=> Returns the value of a ^ b
+=> You are not allowed to import any module
+
+"""
+
+
+def pow(a: int, b: int) -> int:
+    return a**b
