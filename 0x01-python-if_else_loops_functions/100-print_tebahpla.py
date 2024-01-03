a4d05d9438599d506e5a92c7b6d2ba1c6ff1bf73
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 05:37:38 +0300

    fix(style): add necessary spaces, remove unnecessary ones

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@
 def print_alpha(number: int):
     turn = 1
 
-    while(number > 64 and number != 96):
+    while (number > 64 and number != 96):
         if turn == 1:
             print("{char:c}".format(char=number), end="")
             number = (number - 32) - 1
@@ -12,5 +12,6 @@ def print_alpha(number: int):
             print("{char:c}".format(char=number), end="")
             number = (number + 32) - 1
             turn = 1
+
 
 print_alpha(122)
