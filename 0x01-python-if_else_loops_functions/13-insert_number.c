5c5ac2acd6bd4d0d2a32156cb13167675769c3d0
George Ikigu <ikigu.geo@gmail.com>
2024-01-03 04:39:36 +0300

    solution: insert number in sorted singly-linked list
    Number should go where list will remain sorted

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * insert_node - inserts a node into a sorted linked list
+ * @head: the head of the linked list
+ * @number: the number to insert
+ *
+ * Return: the address of the new node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *copy_of_current_previous, *previous, *current, *new_node;
+
+	if ((*head)->next == NULL || *head == NULL)
+	{
+		new_node = add_nodeint_end(head, number);
+		return (new_node);
+	}
+
+	new_node = malloc(sizeof(listint_t));
+	previous = current = *head;
+
+	if (!new_node)
+		return (NULL);
+
+	while (current)
+	{ 
+		if (number <= current->n)
+		{
+			copy_of_current_previous = previous->next;
+			previous->next = new_node;
+			new_node->next = copy_of_current_previous;
+			new_node->n = number;
+			return (new_node);
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	free(new_node);
+	new_node = add_nodeint_end(head, number);
+	printf("I added");
+	return (new_node);
+}
+
