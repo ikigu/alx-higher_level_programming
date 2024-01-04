63767d885b17aaa116488376ed870270ca56c88f
George Ikigu <ikigu.geo@gmail.com>
2024-01-04 18:37:16 +0300

    fix(style): remove trailing whitespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,7 +8,7 @@ Write a program that prints the result of the addition of all arguments
 
 => The output should be the result of the addition of all arguments,
    followed by a new line
-=> You can cast arguments into integers by using int() 
+=> You can cast arguments into integers by using int()
    (you can assume that all arguments can be casted into integers)
 => Your code should not be executed when imported
 """
