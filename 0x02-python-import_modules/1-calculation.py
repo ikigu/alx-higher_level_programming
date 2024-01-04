44529d7219f5bfa59384906f01c9835171e72975
George Ikigu <ikigu.geo@gmail.com>
2024-01-04 17:56:55 +0300

    refactor: remove type annotations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
 
-    a: int = 10
-    b: int = 5
+    a = 10
+    b = 5
 
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
