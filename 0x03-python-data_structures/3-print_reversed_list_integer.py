1683af592f4f939d7b987836286b423bb5fb12b1
George Ikigu <ikigu.geo@gmail.com>
2024-01-08 02:58:27 +0300

    fix: check that list is not empty before iterating

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
 
-    for number in my_list:
-        print("{:d}".format(number))
+    if my_list:
+        for number in my_list:
+            print("{:d}".format(number))
