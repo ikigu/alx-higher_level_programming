3eda8f9ae0dd251def7041e1850303b77fbbdf8b
George Ikigu <ikigu.geo@gmail.com>
2024-01-08 13:57:47 +0300

    fix: correct formatting

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for list in matrix:
-        for number in list:
-            print("{:d}".format(number), end=" ")
+    if len(matrix[0]) == 0 and len(matrix) == 1:
         print("")
+
+    for list in matrix:
+        for i in range(0, len(list)):
+            if i < len(list) - 1:
+                print("{:d}".format(list[i]), end=" ")
+            else:
+                print("{:d}".format(list[i]))
