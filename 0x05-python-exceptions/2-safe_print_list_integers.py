d73e000325b3a4075475249c5777114092eaecd0
George Ikigu <ikigu.geo@gmail.com>
2024-01-22 18:05:57 +0300

    fix: won't handle IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[y]), end="")
             y = y + 1
             z = z + 1
-        except (IndexError, ValueError, TypeError):
+        except (ValueError, TypeError):
             y = y + 1
             continue
 
