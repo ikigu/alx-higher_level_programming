3d9d4331debdd3c9aadd6bd91d3c24ed03ea30d5
George Ikigu <ikigu.geo@gmail.com>
2024-01-22 17:25:55 +0300

    fix: add error type to exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[y]), end="")
             y = y + 1
-        except:
+        except IndexError:
             break
 
     print("")
