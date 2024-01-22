5460d5e10838c96dcc2775e53d422627988542c2
George Ikigu <ikigu.geo@gmail.com>
2024-01-22 18:03:56 +0300

    solution: function that prints only integers in list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    y = 0
+    z = 0
+
+    while y < x:
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            y = y + 1
+            z = z + 1
+        except (IndexError, ValueError, TypeError):
+            y = y + 1
+            continue
+
+    print("")
+    return z
