cfbd037f3739572e9a48d5eca120a57457b3023f
George Ikigu <ikigu.geo@gmail.com>
2024-01-23 22:33:50 +0300

    solution

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square.
+
+        Args:
+            param1 (int, optional): The size of the side of the square.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """int: size of the side of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns:
+            The area of the square
+        """
+        return self.size * self.size
+
+    def my_print(self):
+        """Prints the square with # characters"""
+
+        if self.size == 0:
+            print("")
+            return
+
+        for _ in range(self.size):
+            for _ in range(self.size):
+                print("#", end="")
+            print("")
