6a211dd287bace532084ea6398d8d61d8fda5ed6
George Ikigu <ikigu.geo@gmail.com>
2024-01-23 21:20:34 +0300

    solution: create class Square with private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size):
+        """initializes the square
+
+        Args:
+            param1 (int): the size of the side of the square
+
+        """
+        self.__size = size
