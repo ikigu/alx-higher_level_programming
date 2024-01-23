9af099604bfb4c2cdea6e0f962ae1b1093b3ce23
George Ikigu <ikigu.geo@gmail.com>
2024-01-23 22:11:15 +0300

    solution

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+	"""Defines a square."""
+
+	def __init__(self, size=0):
+		"""Initializes the square.
+
+        Args:
+            param1 (int, optional): The size of the side of the square.
+        """
+		
+		self.size = size
+
+	@property
+	def size(self):
+		"""int: size of the side of the square"""
+		return self.__size
+	
+	@size.setter
+	def size(self, size):
+		if type(size) is int:
+			if size < 0:
+				raise ValueError("size must be >= 0")
+				return
+			self.__size = size
+		else:
+			raise TypeError("size must be an integer")
+		
+
+		
+
+	
