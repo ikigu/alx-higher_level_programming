5ade36eb306217419706e438bc037b211ab9e4a9
George Ikigu <ikigu.geo@gmail.com>
2024-01-29 18:51:15 +0300

    solution: getters and setters for Rectangle class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""module creates a Rectangle class that does nothing"""
+
+
+class Rectangle:
+    """defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initiates the rectangle
+
+        Args:
+            param1 (int): the width of the rectangle
+            param2 (int): the height of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width of a Rectangle instance"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of a Rectangle instance"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the width of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
