45beb8df27a939081f80b3e22ce83282c8136faf
George Ikigu <ikigu.geo@gmail.com>
2024-02-05 23:47:24 +0300

    solution: task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Creates an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """An empty class"""
+
+    def area(self):
+        """Raises an Exception that area is not implemented"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer > 0
+
+        Args:
+            name (str): name of value
+            value (int): value
+        """
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+            return
+
+        if value < 1:
+            raise ValueError(f"{name} must be greater than 0")
