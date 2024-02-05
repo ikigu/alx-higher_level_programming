2275a07927171e37d7ef9ab773aed84f6e192343
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 01:11:24 +0300

    solution: task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Creates a Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Creates Square class, inheriting from Rectangle"""
+
+    def __init__(self, size):
+        try:
+            self.integer_validator("size", size)
+        finally:
+            self.__size = size
+
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return printable string"""
+
+        return f"[Square] {self.__size}/{self.__size}"
