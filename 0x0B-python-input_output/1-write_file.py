f62bc3b9b60be47cbda363029e10502735a4beb0
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 17:17:02 +0300

    fix (__doc__): add function documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ and returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
+    """Writes a string to filename"""
     with open(filename, 'w', encoding='utf-8') as f:
         chars_written = f.write(text)
 
