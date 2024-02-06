030b097dbef8743acdb9c88b182c7594bff8bd29
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 19:14:13 +0300

    solution: task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""adds arguments to python list then saves them to file"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_args():
+    """adds args to file"""
+
+    try:
+        obj = load_from_json_file('add_item.json')
+    except _:
+        obj = []
+
+    for i in range(1, len(sys.argv)):
+        obj.append(sys.argv[i])
+
+    save_to_json_file(obj, 'add_item.json')
+
+
+if __name__ == "__main__":
+    add_args()
