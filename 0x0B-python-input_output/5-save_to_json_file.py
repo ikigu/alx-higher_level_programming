a18ec01eb358657c1798897445c016dcff1b13d3
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 17:57:14 +0300

    solution: task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Function that writes an Object to text file,
+using JSON representation"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json_text = json.dumps(my_obj)
+
+        f.write(json_text)
