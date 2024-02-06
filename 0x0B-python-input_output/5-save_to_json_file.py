a9e23c5cf31dece410a1a816b4fc9536f1062ec9
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 18:01:42 +0300

    fix: remove invalid syntax

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes object to text file as JSON"""
-    `
+
     with open(filename, 'w', encoding='utf-8') as f:
         json_text = json.dumps(my_obj)
 
