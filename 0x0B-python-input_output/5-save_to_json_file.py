7a22d9bada87bd0890544adca5f1710520b67426
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 17:58:54 +0300

    fix (docs): add function documentation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,8 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Writes object to text file as JSON"""
+    `
     with open(filename, 'w', encoding='utf-8') as f:
         json_text = json.dumps(my_obj)
 
