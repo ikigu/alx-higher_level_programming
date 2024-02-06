39329c22d1537468f27e0dcc1fdb25a9e5158eac
George Ikigu <ikigu.geo@gmail.com>
2024-02-06 16:44:10 +0300

    solution: task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Creates a function that reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
