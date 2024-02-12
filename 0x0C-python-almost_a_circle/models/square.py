d75b14f1ab6d9077b58c9422c889f2463ce7d58d
George Ikigu <ikigu.geo@gmail.com>
2024-02-12 20:53:34 +0300

    feat: add square init

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
+
 """Square class"""
 
 
-from models.base import Base
+from models.rectangle import Rectangle
 
 
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Square initialize"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return string representation of square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
