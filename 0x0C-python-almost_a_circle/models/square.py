296d0bf9c94b8c72068fcb24a02ea42e782a35d5
George Ikigu <ikigu.geo@gmail.com>
2024-02-12 21:56:34 +0300

    feat: add square update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,9 +20,33 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """width and height getter"""
         return self.width
 
     @size.setter
     def size(self, size):
+        """width and height setter"""
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle"""
+
+        if len(args) > 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        elif len(kwargs) > 0:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                elif key == "size":
+                    self.size = kwargs[key]
+                elif key == "x":
+                    self.x = kwargs[key]
+                else:
+                    self.y = kwargs[key]
