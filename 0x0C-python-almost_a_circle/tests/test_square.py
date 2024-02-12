ac65b14044cfa05154acda0e3f79ebce19c616e9
George Ikigu <ikigu.geo@gmail.com>
2024-02-12 22:38:13 +0300

    test: add test for Square to dict method

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -10,13 +10,13 @@ from unittest.mock import patch
 
 class TestsquareClass(unittest.TestCase):
     def test_doc(self):
+        """Test documentation"""
         self.assertTrue(len(Square.__doc__) > 1)
         self.assertTrue(len(models.square.__doc__) > 1)
         self.assertTrue(len(Square.__init__.__doc__) > 1)
-        # self.assertTrue(len(Square.__str__.__doc__) > 1)
-        # self.assertTrue(len(Square.size.__doc__) > 1)
 
     def test_square_attributes(self):
+        """Test attributes"""
         s1 = Square(9, 7, 8)
 
         self.assertEqual(s1.id, 1)
@@ -28,6 +28,7 @@ class TestsquareClass(unittest.TestCase):
         del s1
 
     def test_deletion(self):
+        """Test deletion"""
         s1 = Square(1)
         self.assertEqual(s1.id, 1)
         del s1
@@ -37,10 +38,12 @@ class TestsquareClass(unittest.TestCase):
         del s2
 
     def test_no_attributes(self):
+        """Test no attributes"""
         with self.assertRaises(TypeError):
             s1 = Square()
 
     def test_str(self):
+        """test __str__"""
         r3 = Square(3, 4, 5)
 
         self.assertEqual(r3.__str__(), "[Square] (1) 4/5 - 3")
@@ -48,6 +51,7 @@ class TestsquareClass(unittest.TestCase):
         del r3
 
     def test_integer_validation(self):
+        """Test validation"""
         with self.assertRaises(TypeError) as context:
             r1 = Square("9")
         self.assertEqual(context.exception.args[0], "width must be an integer")
@@ -70,6 +74,7 @@ class TestsquareClass(unittest.TestCase):
         self.assertEqual(98, r1.size)
 
     def test_update_0(self):
+        """Test update"""
         r1 = Square(10, 10, 10, 10)
         self.assertEqual(r1.__str__(), "[Square] (10) 10/10 - 10")
 
@@ -106,3 +111,21 @@ class TestsquareClass(unittest.TestCase):
         self.assertEqual(r1.__str__(), "[Square] (214) 738/3 - 67")
 
         del r1
+
+    def test_to_dictionary(self):
+        s1 = Square(10, 2, 1, 9)
+        s2 = Square(2, 2)
+        s1_todict = s1.to_dictionary()
+
+        self.assertEqual(
+            s1_todict, {'id': 9, 'size': 10, 'x': 2, 'y': 1})
+        self.assertEqual(s2.__str__(), "[Square] (1) 2/0 - 2")
+        self.assertEqual(type(s1_todict), dict)
+
+        s2.update(**s1_todict)
+        self.assertEqual(s1.__str__(), s2.__str__())
+        self.assertNotEqual(s1, s2)
+        self.assertEqual(s1_todict, s2.to_dictionary())
+
+        del s1
+        del s2
