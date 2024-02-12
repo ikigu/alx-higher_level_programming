7a8d261cd564809a65a6bd44f7a237d72a16ce6b
George Ikigu <ikigu.geo@gmail.com>
2024-02-12 19:28:35 +0300

    feat: add base model

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""My Base class"""
+
+
+class Base():
+    """The Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The class constructor"""
+
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    def __del__(self):
+        if Base.__nb_objects >= 1:
+            Base.__nb_objects -= 1
+
+        del self
