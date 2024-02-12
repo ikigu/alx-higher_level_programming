42dc9344e9bbe949e5658030a5590c23851e58cc
George Ikigu <ikigu.geo@gmail.com>
2024-02-12 20:53:54 +0300

    tests: add square tests

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,39 @@
+"""Tests square class"""
+
+
+import unittest
+from models.square import Square
+import models.square
+from io import StringIO
+from unittest.mock import patch
+
+
+class TestsquareClass(unittest.TestCase):
+    def test_doc(self):
+        self.assertTrue(len(Square.__doc__) > 1)
+        self.assertTrue(len(models.square.__doc__) > 1)
+        self.assertTrue(len(Square.__init__.__doc__) > 1)
+
+    def test_square_attributes(self):
+        s1 = Square(9, 7, 8)
+
+        self.assertEqual(s1.id, 1)
+        self.assertEqual(s1.height, 9)
+        self.assertEqual(s1.width, s1.height)
+        self.assertEqual(s1.x, 7)
+        self.assertEqual(s1.y, 8)
+
+        del s1
+
+    def test_deletion(self):
+        s1 = Square(1)
+        self.assertEqual(s1.id, 1)
+        del s1
+
+        s2 = Square(2)
+        self.assertEqual(s2.id, 1)
+        del s2
+
+    def test_no_attributes(self):
+        with self.assertRaises(TypeError):
+            s1 = Square()
