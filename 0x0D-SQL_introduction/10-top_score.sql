357776d2905d4d539e335c75db20ff0e16698cd6
George Ikigu <ikigu.geo@gmail.com>
2024-03-12 19:12:37 +0300

    answer: show second table, order by score desc

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of second_table
+-- Results display both score and name, in that order
+-- Records are are order by score, top first
+SELECT score, name FROM second_table ORDER BY score DESC;
