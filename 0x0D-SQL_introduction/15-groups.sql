59a45a4da7cb8fa4dfebc39faebc75fec0b5a946
George Ikigu <ikigu.geo@gmail.com>
2024-03-12 19:35:59 +0300

    answer: lists number of record with same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- List the number of records with the same score in second_table
+-- Show number of records with label number
+SELECT score, COUNT(score) AS number
+    FROM second_table
+    GROUP BY score
+    ORDER BY score DESC;
