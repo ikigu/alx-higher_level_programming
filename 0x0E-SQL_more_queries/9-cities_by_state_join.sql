303d0b066987c87af47b5bec655b3bce85e3459d
Ikigu <ikigu.geo@gmail.com>
2024-03-13 21:03:32 +0300

    answer: list cities plus their state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
 -- list all cities in hbtn_0d_usa
 --      sort results in ascending order by cities.id
 --      use only one select statement
+SELECT cities.id, cities.name, states.name FROM cities
+  JOIN states ON cities.state_id = states.id
+ ORDER BY cities.id;
