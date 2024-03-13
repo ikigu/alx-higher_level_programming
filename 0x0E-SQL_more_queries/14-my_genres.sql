95012667bb392ef67d9aab5114785043df450080
George Ikigu <ikigu.geo@gmail.com>
2024-03-14 00:55:13 +0300

    fix: add order by clause

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ SELECT `name` FROM tv_genres
     ON tv_genres.id = tv_show_genres.genre_id
   LEFT JOIN tv_shows
     ON tv_shows.id = tv_show_genres.show_id
- WHERE tv_shows.title = 'Dexter';
+ WHERE tv_shows.title = 'Dexter'
+ ORDER BY `name`;
