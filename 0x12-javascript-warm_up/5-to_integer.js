529341ae1ec7ee8e4d68c4b010307fc1bb9f8d67
Ikigu <ikigu.geo@gmail.com>
2024-04-08 17:43:07 +0300

    solution: task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstArgumentAsNumber = parseInt(process.argv[2]);
+
+if (Number.isNaN(firstArgumentAsNumber)) {
+  console.log('Not a number');
+} else {
+	console.log(firstArgumentAsNumber);
+}
