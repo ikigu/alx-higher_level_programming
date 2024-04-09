9e1ff74167d61964f0a424cf1d59438ae1f7f12f
George Ikigu <ikigu.geo@gmail.com>
2024-04-09 11:39:28 +0300

    style: river

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- list all shows and their genre ids
 SELECT s.title, g.genre_id
   FROM tv_shows AS s
-  LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
+  LEFT JOIN tv_show_genres AS g
+    ON s.id = g.show_id
  ORDER BY s.title, g.genre_id;
