210023efe730ae5b9aaaa616b5046b10bb8e9677
Ikigu <ikigu.geo@gmail.com>
2024-04-11 11:18:05 +0300

    fix: turn user input into complete string

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,18 +13,15 @@ if __name__ == "__main__":
     USER = sys.argv[1]
     PASSWORD = sys.argv[2]
     DATABASE_NAME = sys.argv[3]
-    state_name = sys.argv[4]
-
-    state_name = state_name.replace("'", "")
-    state_name = state_name.replace(";", "")
+    STATE_NAME = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=USER, passwd=PASSWORD, db=DATABASE_NAME)
     cursor = db.cursor()
-    cursor.execute(f"SELECT name FROM cities \
+    cursor.execute("SELECT name FROM cities \
                     WHERE state_id = (SELECT id FROM states \
-                    WHERE name = '{state_name}') \
-                    ORDER BY cities.id ASC")
+                    WHERE name = %(state)s) \
+                    ORDER BY cities.id ASC", {'state': STATE_NAME})
 
     query_rows = cursor.fetchall()
 
