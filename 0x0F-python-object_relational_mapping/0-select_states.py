c3e42f99d976fc347ed16dcf8c42d76a16fadba9
Ikigu <ikigu.geo@gmail.com>
2024-04-11 08:42:34 +0300

    fix: add shebang and give right index to cli arguments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+
 """This module lists all states from the hbtn_0e_0_usa database"""
 
 if __name__ == "__main__":
     import sys
     import MySQLdb
 
-    USER = sys.argv[0]
-    PASSWORD = sys.argv[1]
-    DATABASE_NAME = sys.argv[2]
+    USER = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+
+    print(USER)
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=USER, passwd=PASSWORD, db=DATABASE_NAME)
