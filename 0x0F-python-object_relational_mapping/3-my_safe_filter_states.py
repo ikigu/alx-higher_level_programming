4fd9880bc6d5b7ee2aec345cb45e0690a763f9b5
Ikigu <ikigu.geo@gmail.com>
2024-04-11 10:19:39 +0300

    solution: task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This module lists all states from the hbtn_0e_0_usa database
+whose name is what the user passes as the fourth argument.
+The user input is validated to guard against SQL injections.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    USER = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+    state_name = sys.argv[4]
+
+    state_name = state_name.split("'")[0]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=USER, passwd=PASSWORD, db=DATABASE_NAME)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE BINARY "{}" \
+                    ORDER BY id ASC'.format(state_name))
+
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db.close()
