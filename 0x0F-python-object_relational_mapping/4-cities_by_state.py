d39c1e79d6891592b9d0f16e890c678946c22aa6
Ikigu <ikigu.geo@gmail.com>
2024-04-11 10:58:09 +0300

    solution: task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This module lists all cities and corresponding state
+from the hbtn_0e_0_usa database.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    USER = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=USER, passwd=PASSWORD, db=DATABASE_NAME)
+    cursor = db.cursor()
+    cursor.execute('SELECT cities.id, cities.name, states.name \
+                   FROM cities JOIN states WHERE cities.state_id = states.id')
+
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db.close()
