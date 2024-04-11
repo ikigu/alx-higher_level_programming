15f9c7a71de371d6de38db429baab42494d48632
Ikigu <ikigu.geo@gmail.com>
2024-04-11 09:02:48 +0300

    solution: task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This module lists all states
+from the hbtn_0e_0_usa database that begin with N
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    USER = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=USER, passwd=PASSWORD, db=DATABASE_NAME)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db.close()
