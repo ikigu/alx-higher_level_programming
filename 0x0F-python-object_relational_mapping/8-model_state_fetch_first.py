e39bd892749976550c382fc2806b2f10c2bb050f
Ikigu <ikigu.geo@gmail.com>
2024-04-12 10:19:35 +0300

    solution: task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -39,5 +39,5 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
-        print(f'{instance.id}: {instance.name}')
+    for instance in session.query(State).where(State.id==1):
+        print(f"{instance.id}: {instance.name}")
