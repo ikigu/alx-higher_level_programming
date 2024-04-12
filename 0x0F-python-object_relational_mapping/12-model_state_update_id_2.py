094d5511f255ea1a719dbd35230e674a979ac0d3
Ikigu <ikigu.geo@gmail.com>
2024-04-12 11:57:57 +0300

    fix: update name to New Mexico not Mexico

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -37,6 +37,5 @@ if __name__ == '__main__':
     session = Session()
 
     state = session.query(State).filter_by(id=2).first()
-    state.name = 'Mexico'
+    state.name = 'New Mexico'
     session.commit()
-    session.close()
