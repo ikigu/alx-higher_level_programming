e162d04d258a5f4bb14d233971ed12af65d672d5
Ikigu <ikigu.geo@gmail.com>
2024-04-25 17:27:03 +0300

    fix (reqs): limit script to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response from a URL
-
-url=$1
-response=$(curl -sI "$url" | grep 'Content-Length' | cut -d ':' -f 2)
-echo "$response"
+echo $(curl -sI "$1" | grep 'Content-Length' | cut -d ':' -f 2)
