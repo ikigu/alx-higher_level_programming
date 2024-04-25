35d51f1a8face1a6a6738f77bf7de91d0daa569d
Ikigu <ikigu.geo@gmail.com>
2024-04-25 19:05:47 +0300

    solution: task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# sends a POST request to the passed URL, and displays the body of the response; A variable email must be sent with the value test@gmail.com; A variable subject must be sent with the value I will always be here for PLD
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
