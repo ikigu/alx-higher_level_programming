3a8b6bfdb8889ac80882e27388768e54a0f80a03
George Ikigu <ikigu.geo@gmail.com>
2024-04-26 18:06:14 +0300

    solution: task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    headers = {'Accept': 'application/vnd.github+json'}
+
+    response = requests.get(
+        f'https://api.github.com/repos/{owner}/{repo}/commits',
+        headers=headers,)
+
+    if response.ok:
+        response = response.json()
+
+        for i in range(0, 10):
+
+            sha_code = response[i]["sha"]
+            author = response[i]["commit"]["author"]["name"]
+
+            print(
+                f'{sha_code}: {author}')
+    else:
+        print(None)
