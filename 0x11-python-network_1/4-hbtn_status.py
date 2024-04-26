25be9dfe0efafc75253e4c5738381f31744f7c69
George Ikigu <ikigu.geo@gmail.com>
2024-04-26 10:33:09 +0300

    solution: task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""
+Fetches https://alx-intranet.hbtn.io/status
+
+Requirements:
+    - You must use the package requests
+    - You are not allow to import packages other than requests
+    - The body of the response must be display
+      like the following example (tabulation before -)
+"""
+
+if __name__ == '__main__':
+    from requests import get
+
+    r = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print(f'\t- type: {type(r.text)}')
+    print(f'\t- content: {r.text}')
